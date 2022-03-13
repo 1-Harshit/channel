43f099f32183fd0838e25a46b39e715cd137b331
Harshit Raj <harshitr20@iitk.ac.in>
2022-03-14 00:38:52 +0530

    Add callback

---
+++ web/src/api/callbacks.ts
@@ -0,0 +1,196 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from "axios";
+
+const BASE_URL = "SOMETHING";
+const ERROR_MESSAGE = "Error in API CALL";
+
+// Interfaces
+export interface SignupParams {
+  Name: string;
+  Username: string;
+  Password: string;
+}
+
+export interface LoginParams {
+  Username: string;
+  Password: string;
+}
+
+export interface ChannelParams {
+  Name: string;
+  Description: string;
+}
+
+export interface MessageParams {
+  Content: string;
+  ChannelId: string;
+}
+
+export interface Response {
+  Payload?: any;
+  Status: any;
+}
+
+// API
+const postSignup = async (params: SignupParams): Promise<Response> => {
+  let status;
+  await axios
+    .post(BASE_URL + "/user/signup", {
+      Name: params.Name,
+      Password: params.Password,
+      Username: params.Username,
+    })
+    .then((res) => {
+      status = res.status;
+    })
+    .catch((err) => {
+      status = err?.response?.status ?? 500;
+    });
+  const response: Response = {
+    Status: status,
+  };
+  return response;
+};
+
+const postLogin = async (params: LoginParams): Promise<Response> => {
+  let payload = "",
+    status;
+  await axios
+    .post(BASE_URL + "/user/login", {
+      RollNo: params.Username,
+      Password: params.Password,
+    })
+    .then((res) => {
+      payload = res.data?.token;
+      localStorage.setItem("token", payload);
+      status = res.status;
+    })
+    .catch((err) => {
+      payload = err?.response?.data.error ?? ERROR_MESSAGE;
+      status = err?.response?.status ?? 500;
+    });
+
+  const response: Response = {
+    Payload: payload,
+    Status: status,
+  };
+
+  return response;
+};
+
+const postChannel = async (channelParams: ChannelParams): Promise<Response> => {
+  const token = localStorage.getItem("token");
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  const bodyParameters = {
+    Name: channelParams.Name,
+    Description: channelParams.Description,
+  };
+
+  let status;
+  await axios
+    .post(BASE_URL + "/channel", bodyParameters, config)
+    .then((res) => {
+      status = res.status;
+    })
+    .catch((err) => {
+      status = err?.response?.status ?? 500;
+    });
+
+  const response: Response = {
+    Status: status,
+  };
+
+  return response;
+};
+
+const getChannels = async (): Promise<Response> => {
+  const token = localStorage.getItem("token");
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  let status, payload;
+  await axios
+    .get(BASE_URL + "/channels", config)
+    .then((res) => {
+      payload = res.data;
+      status = res.status;
+    })
+    .catch((err) => {
+      payload = err?.response?.data.error ?? ERROR_MESSAGE;
+      status = err?.response?.status ?? 500;
+    });
+
+  const response: Response = {
+    Payload: payload,
+    Status: status,
+  };
+  return response;
+};
+
+const getMessages = async (channelId: string): Promise<Response> => {
+  const token = localStorage.getItem("token");
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  let status, payload;
+  await axios
+    .get(BASE_URL + `/channel/${channelId}/messages`, config)
+    .then((res) => {
+      payload = res.data;
+      status = res.status;
+    })
+    .catch((err) => {
+      payload = err?.response?.data.error ?? ERROR_MESSAGE;
+      status = err?.response?.status ?? 500;
+    });
+
+  const response: Response = {
+    Payload: payload,
+    Status: status,
+  };
+  return response;
+};
+
+const postMessage = async (message: MessageParams): Promise<Response> => {
+  const token = localStorage.getItem("token");
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  const bodyParameters = {
+    Content: message.Content,
+    SentAt: +new Date(),
+  };
+
+  let status, payload;
+  await axios
+    .post(BASE_URL + `channel/${message.ChannelId}/message`, bodyParameters, config)
+    .then((res) => {
+      payload = res.data;
+      status = res.status;
+    })
+    .catch((err) => {
+      payload = err?.response?.data.error ?? ERROR_MESSAGE;
+      status = err?.response?.status ?? 500;
+    });
+
+  const response: Response = {
+    Payload: payload,
+    Status: status,
+  };
+  return response;
+};
+
+export {
+  postSignup,
+  postLogin,
+  postChannel,
+  getChannels,
+  getMessages,
+  postMessage,
+};
