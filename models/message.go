9e4d9da18c2b4bffadb30fccb4e96dbac07dcfda
Bhuvan Singla <bhuvansingla2000@gmail.com>
2022-04-12 00:24:15 +0530

    Fix message models

--- models/message.go
+++ models/message.go
@@ -1,20 +1,22 @@
 package models
 
 type Message struct {
-	ID         uint   `gorm:"primary_key" json:"id"`
-	Content    string `json:"content"`
-	TimeSentAt string `json:"sentAt"`
-	Username   string `json:"userId"`
-	ChannelId  string `json:"channelId"`
+	ID           uint    `gorm:"primary_key" json:"id"`
+	Content      string  `json:"content"`
+	TimeSentAt   int64   `json:"sentAt"`
+	UserUsername string  `json:"sentByUsername"`
+	User         User    `gorm:"foreignkey:UserUsername" json:"-"`
+	ChannelName  string  `json:"channelName"`
+	Channel      Channel `gorm:"foreignkey:ChannelName" json:"-"`
 }
 
-func CreateMessage(content string, timeSentAt string, username string, channelId string) error {
-	err := db.Create(&Message{Content: content, TimeSentAt: timeSentAt, Username: username, ChannelId: channelId}).Error
+func CreateMessage(content string, timeSentAt int64, username string, channelId string) error {
+	err := db.Create(&Message{Content: content, TimeSentAt: timeSentAt, UserUsername: username, ChannelName: channelId}).Error
 	return err
 }
 
-func GetMessages(channelId string) ([]Message, error) {
+func GetMessages(channelName string) ([]Message, error) {
 	var messages []Message
-	err := db.Where("channel_id = ?", channelId).Find(&messages).Error
+	err := db.Where("channel_name = ?", channelName).Find(&messages).Error
 	return messages, err
 }
