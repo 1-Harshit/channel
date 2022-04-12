f0d95c41cd54dc086acd53dff3fd65d508fa1861
Bhuvan Singla <bhuvansingla2000@gmail.com>
2022-04-12 11:41:32 +0530

    Add CORS policy

--- routes/routes.go
+++ routes/routes.go
@@ -23,17 +23,17 @@ func RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/", controllers.Index).Methods("GET")
 
 	// Channel
-	router.HandleFunc("/channel", middleware.IsAuthorized(controllers.CreateChannel)).Methods("POST")
-	router.HandleFunc("/channels", middleware.IsAuthorized(controllers.GetAllChannels)).Methods("GET")
-	router.HandleFunc("/channel/{channelId}", middleware.IsAuthorized(controllers.DeleteChannel)).Methods("DELETE")
+	router.HandleFunc("/channel", middleware.CORS(middleware.IsAuthorized(controllers.CreateChannel))).Methods("POST")
+	router.HandleFunc("/channels", middleware.CORS(middleware.IsAuthorized(controllers.GetAllChannels))).Methods("GET")
+	router.HandleFunc("/channel/{channelId}", middleware.CORS(middleware.IsAuthorized(controllers.DeleteChannel))).Methods("DELETE")
 
 	// Message
-	router.HandleFunc("/channel/{channelId}/message", middleware.IsAuthorized(controllers.CreateMessage)).Methods("POST")
-	router.HandleFunc("/channel/{channelId}/messages", middleware.IsAuthorized(controllers.GetMessages)).Methods("GET")
+	router.HandleFunc("/channel/{channelId}/message", middleware.CORS(middleware.IsAuthorized(controllers.CreateMessage))).Methods("POST")
+	router.HandleFunc("/channel/{channelId}/messages", middleware.CORS(middleware.IsAuthorized(controllers.GetMessages))).Methods("GET")
 
 	// User
-	router.HandleFunc("/user/login", controllers.Login).Methods("POST")
-	router.HandleFunc("/user/signup", controllers.Signup).Methods("POST")
-	router.HandleFunc("/users", middleware.IsAuthorized(controllers.ListAllUsers)).Methods("GET")
-	router.HandleFunc("/user/{userId}", middleware.IsAuthorized(controllers.GetUser)).Methods("GET")
+	router.HandleFunc("/user/login", middleware.CORS(controllers.Login)).Methods("POST")
+	router.HandleFunc("/user/signup", middleware.CORS(controllers.Signup)).Methods("POST")
+	router.HandleFunc("/users", middleware.CORS(middleware.IsAuthorized(controllers.ListAllUsers))).Methods("GET")
+	router.HandleFunc("/user/{userId}", middleware.CORS(middleware.IsAuthorized(controllers.GetUser))).Methods("GET")
 }
